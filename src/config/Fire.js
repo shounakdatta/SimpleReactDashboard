29d913b04eaf81d76d3353597a69129e11297e1d
Shounak Datta <33079683+shounakdatta@users.noreply.github.com>
2019-08-17 17:21:59 -0400

    Update Fire.js

--- src/config/Fire.js
+++ src/config/Fire.js
@@ -1,15 +1,7 @@
 import firebase from "firebase";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyB8zpMJjCdgJOS4jRA4VX-5g9VlFde2oqk",
-  authDomain: "pricetracker-b7847.firebaseapp.com",
-  databaseURL: "https://pricetracker-b7847.firebaseio.com",
-  projectId: "pricetracker-b7847",
-  storageBucket: "",
-  messagingSenderId: "868808958925",
-  appId: "1:868808958925:web:b6926952e1fd568e"
-};
+const firebaseConfig = {};
 
 // Initialize Firebase
 const fire = firebase.initializeApp(firebaseConfig);
